02c81b52ba371592ed41b868295a4ced8347335c
Onele Nkangezi <nknone001@uct.ac.za>
2023-08-03 21:55:55 +0200

    Added search parallel version. It produces an ArrayIndexOutOfBoundsException when terrain.mark_visited(). Need to fixx this error

--- src/SearchParallel.java
+++ src/SearchParallel.java
@@ -1,14 +1,15 @@
 import java.util.Random;
 import java.util.concurrent.ForkJoinTask;
+
 public class SearchParallel extends java.util.concurrent.RecursiveTask<Integer> {
     private SearchParallel[] arr;
     private int id;
     private int posX,posY;
-    private TerrainArea terrain;
+    static private TerrainArea terrain;
     boolean stopped;
-    private int steps=0;
+    private int steps;
 
-    final Integer CUT_OFF = 500;
+    final Integer CUT_OFF = 100;
 
 //does it have to be static?Yes I think.
 
@@ -17,36 +18,43 @@ public class SearchParallel extends java.util.concurrent.RecursiveTask<Integer> 
         this.arr = arr;
 		this.posX = pos_row; //randomly allocated
 		this.posY = pos_col; //randomly allocated
-        this.terrain = terrain;
+        SearchParallel.terrain = terrain;
 
 		this.stopped = false;
 	}
     @Override
     //Returns the height of the minimum valley calculated
     protected Integer compute() {
-        if (arr.length<CUT_OFF){
-            Random rand = new Random();
-            SearchParallel searcher;
-            int x = (int)terrain.xmax;
-            int y = (int)terrain.ymax;
+        Random rand = new Random();
+        if (arr.length<=CUT_OFF){
             int local_min=Integer.MAX_VALUE;
-            for (int i=0;i<arr.length;i++){
-                searcher = new SearchParallel(null,i,rand.nextInt(x),rand.nextInt(y),terrain); 
+            for (int i=0;i<arr.length;){
+                i++;
                 int temp = local_min;
-                local_min = searcher.find_valleys();
+                local_min = arr[i].find_valleys();
                 if(local_min>temp){
                     local_min = temp;
                 }
                 return local_min;
             }
-            ForkJoinTask<Integer> right = (new SearchParallel(arr, x, y, local_min, terrain)).fork();
-            int left = (new SearchParallel(arr, x, y, local_min, terrain)).join();
-            int rightWing = right.join();
-            return Math.min(left,rightWing);
         }
-        
+        int half = (int)arr.length/2;
+        SearchParallel [] siri = new SearchParallel[half];
+        SearchParallel [] bing = new SearchParallel[half];
+        for(int i=0;i<half;i++){
+            siri[i] = arr[i];
+            }
+        int j = 0;
+        for(int i=half;i<arr.length;i++){
+            bing[j] = arr[i];
+            j++;
+            }   
+        ForkJoinTask<Integer> right = (new SearchParallel(siri,j, rand.nextInt(posX-1),rand.nextInt(posY-1), terrain)).fork();
+        int left = (new SearchParallel(bing, rand.nextInt(posX-1),j,rand.nextInt(posY-1), terrain)).compute();
+        int rightWing = right.join();
+        return Math.min(left,rightWing);   
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'compute'");
+        
         
     }
 
@@ -56,7 +64,7 @@ public class SearchParallel extends java.util.concurrent.RecursiveTask<Integer> 
 		while(terrain.visited(posX, posY)==0) { // stop when hit existing path
 			height=terrain.get_height(posX, posY);
 			terrain.mark_visited(posY, posY, id); //mark current position as visited
-			steps++;
+            steps++;
 			next = terrain.next_step(posX, posY);
 			switch(next) {
 				case STAY_HERE: return height; //found local valley
